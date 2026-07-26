f0b3cc3254687e7b6000f2b7ed22df85b9b02483
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2026-07-26 18:00:32 -0300

    exercicio 05 condicionais

---
+++ src/condicionais/ex05.ts
@@ -0,0 +1,23 @@
+/*Verifique se um ano é bissexto.*/
+
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+
+const rl = readline.createInterface({ input, output });
+
+async function main() {
+  const entrada = await rl.question("Digite um ano: ");
+  const ano = Number(entrada);
+
+  if (ano % 4 === 0 && ano % 100 !== 0) {
+    console.log(`${ano} é bissexto!`);
+  } else if (ano % 100 === 0 && ano % 400 === 0) {
+    console.log(`${ano} é bissexto!`);
+  } else {
+    console.log(`${ano} não é bissexto!`);
+  }
+
+  rl.close();
+}
+
+main();
