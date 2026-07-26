0236b216439b715c38dace584a3e5009ad4156ea
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2026-07-26 16:25:29 -0300

    mais configurações e exercicio 01

---
+++ src/condicionais/ex01.ts
@@ -0,0 +1,19 @@
+import * as readline from "node:readline/promises";
+import {stdin as input, stdout as output} from "node:process";
+
+const rl = readline.createInterface({input, output});
+
+async function main(){
+  const entrada = await rl.question("Digite um número: ");
+  const numero = Number(entrada);
+
+  if(numero % 2 === 0){
+    console.log(`${numero} é par.`);
+  }else{
+    console.log(`${numero} é ímpar.`);
+  }
+
+  rl.close();
+}
+
+main();
