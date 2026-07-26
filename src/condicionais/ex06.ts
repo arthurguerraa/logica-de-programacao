94ca4cde5ad9b6f1691a920fe55969cded85342e
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2026-07-26 19:41:11 -0300

    exercicio 06 condicionais

---
+++ src/condicionais/ex06.ts
@@ -0,0 +1,23 @@
+/*Calcule o IMC.*/
+
+import * as readline from "node:readline/promises";
+import {stdin as input, stdout as output} from "node:process";
+
+const rl = readline.createInterface({input, output});
+
+async function main(){
+  const entrada1 = await rl.question("Digite sua altura em metros: ");
+  const altura = Number(entrada1);
+  const entrada2 = await rl.question("Digite seu peso em kilos: ");
+  const peso = Number(entrada2);
+
+  let imc: number = 0;
+
+  imc = peso/altura;
+
+  console.log(`Seu peso: ${peso}\nSua altura: ${altura}\nSeu IMC: ${imc}`);
+
+  rl.close();
+}
+
+main();
