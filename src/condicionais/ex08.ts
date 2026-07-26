e3c68e329e7da178665d9731d1e83f100755da99
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2026-07-26 19:59:57 -0300

    exercicio 08 condicionais

---
+++ src/condicionais/ex08.ts
@@ -0,0 +1,21 @@
+/*Converta Celsius para Fahrenheit.*/
+
+import * as readline from "node:readline/promises";
+import {stdin as input, stdout as output} from "node:process";
+
+const rl = readline.createInterface({input, output});
+
+async function main(){
+  const entrada = await rl.question("Digite a temperatura em Celsius: ");
+  const celsius = Number(entrada);
+
+  let fahrenheit = 0;
+
+  fahrenheit = (((celsius * 9) / 5) + 32);
+
+  console.log(`Temperatura em Celsius: ${celsius}\nTemperatura em Fahrenheit: ${fahrenheit}`);
+
+  rl.close();
+}
+
+main();
