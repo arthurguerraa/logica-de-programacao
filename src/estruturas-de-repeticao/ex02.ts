068391e5c91a397c2acb7234005f4883bba5487d
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2026-07-27 17:32:31 -0300

    exercicio 06 loops

--- src/estruturas-de-repeticao/ex02.ts
+++ src/estruturas-de-repeticao/ex02.ts
@@ -3,7 +3,7 @@
 async function main(){
   let i: number;
   for(i = 1; i <= 100; i++){
-    if(i % 2 == 0){
+    if(i % 2 === 0){
       console.log(i);
     }
   }
---
+++ src/estruturas-de-repeticao/ex06.ts
@@ -0,0 +1,22 @@
+/*Contar quantos números pares existem entre 1 e N.*/
+
+import * as readline from "node:readline/promises";
+import {stdin as input, stdout as output} from "node:process";
+
+const rl = readline.createInterface({input, output});
+
+async function main (){
+  const entrada = await rl.question("Digite um número para descobrir quantos números pares tem entre ele e o número 1: ");
+  const n1 = Number(entrada);
+
+  let i: number;
+  let cont: number = 0;
+  for(i = 1; i <= n1; i++){
+    if(i % 2 === 0){
+      cont++
+    }
+  }
+  console.log(`Entre 1 e ${n1} existem ${cont} números pares`);
+  rl.close();
+}
+main();
