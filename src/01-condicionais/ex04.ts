00b3d53f4b6efa1c9b8e9f04adefd0b364ebdfba
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2026-08-03 15:37:10 -0300

    exercicio 04 corrigido

--- src/01-condicionais/ex04.ts
+++ src/01-condicionais/ex04.ts
@@ -1,4 +1,4 @@
-/*Leia um número e informe se é positivo ou negativo.*/
+/*Descubra se um número é par ou ímpar.*/
 
 import * as readline from "node:readline/promises";
 import {stdin as input, stdout as output} from "node:process";
@@ -9,12 +9,12 @@ async function main(){
   const entrada = await rl.question("Digite um número: ");
   const n1 = Number(entrada);
 
-  if(n1 > 0 ){
-    console.log(`${n1} é positivo`);
-  }else if(n1 < 0 ){
-    console.log(`${n1} é negativo`);
+  if(n1 % 2 === 0 && n1 !== 0){
+    console.log(`${n1} é par!`);
+  }else if(n1 === 0){
+    console.log(`${n1} é 0!`);
   }else{
-    console.log(`${n1} é 0`);
+    console.log(`${n1} é ímpar!`);
   }
   rl.close();
 }
